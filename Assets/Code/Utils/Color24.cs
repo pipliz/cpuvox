15eb90c2842183db6ba446d25baf651fae547bc7
pipliz <pipliznl@gmail.com>
2020-04-18 11:40:05 +0200

    Expand ColorARGB32

--- Assets/Code/Utils/Color24.cs
+++ Assets/Code/Utils/Color24.cs
@@ -1,17 +1,18 @@
-﻿using System.Runtime.InteropServices;
+﻿using System;
+using System.Runtime.InteropServices;
 using UnityEngine;
 
 [StructLayout(LayoutKind.Sequential)]
-public struct ColorARGB32
+public struct ColorARGB32 : IEquatable<ColorARGB32>
 {
-	byte a;
+	public byte a;
 	public byte r;
 	public byte g;
 	public byte b;
 
-	public ColorARGB32 (byte r, byte g, byte b)
+	public ColorARGB32 (byte r, byte g, byte b, byte a = 255)
 	{
-		a = 255;
+		this.a = a;
 		this.r = r;
 		this.g = g;
 		this.b = b;
@@ -19,6 +20,39 @@ public struct ColorARGB32
 
 	public static implicit operator ColorARGB32 (Color32 source)
 	{
-		return new ColorARGB32(source.r, source.g, source.b);
+		return new ColorARGB32(source.r, source.g, source.b, source.a);
+	}
+
+	public static bool operator == (ColorARGB32 aRGB1, ColorARGB32 aRGB2)
+	{
+		return aRGB1.Equals(aRGB2);
+	}
+
+	public static bool operator != (ColorARGB32 aRGB1, ColorARGB32 aRGB2)
+	{
+		return !(aRGB1 == aRGB2);
+	}
+
+	public override bool Equals (object obj)
+	{
+		return obj is ColorARGB32 && Equals((ColorARGB32)obj);
+	}
+
+	public bool Equals (ColorARGB32 other)
+	{
+		return a == other.a &&
+			   r == other.r &&
+			   g == other.g &&
+			   b == other.b;
+	}
+
+	public override int GetHashCode ()
+	{
+		var hashCode = 94257292;
+		hashCode = hashCode * -1521134295 + a.GetHashCode();
+		hashCode = hashCode * -1521134295 + r.GetHashCode();
+		hashCode = hashCode * -1521134295 + g.GetHashCode();
+		hashCode = hashCode * -1521134295 + b.GetHashCode();
+		return hashCode;
 	}
 }
