c21202810e649ae08252ebc8f28bb6dc1b02a9dd
pipliz <pipliznl@gmail.com>
2020-04-23 17:17:25 +0200

    Fixed voxelizer to be an actual thing :o

--- Assets/Code/VoxelizerHelper.cs
+++ Assets/Code/VoxelizerHelper.cs
@@ -36,7 +36,10 @@ public class VoxelizerHelper
 		float3 b = context.verts[i1];
 		float3 c = context.verts[i2];
 		float3 middle = (a + b + c) / 3f;
-		float3 normal = normalize(cross(b - a, c - a));
+		float3 normalTri = normalize(cross(b - a, c - a));
+		a += normalize(a - middle) * 0.5f;
+		b += normalize(b - middle) * 0.5f;
+		c += normalize(c - middle) * 0.5f;
 
 		float3 minf = min(a, min(b, c));
 		float3 maxf = max(a, max(b, c));
@@ -52,18 +55,40 @@ public class VoxelizerHelper
 		for (int x = mini.x; x <= maxi.x; x++) {
 			for (int z = mini.z; z <= maxi.z; z++) {
 				for (int y = mini.y; y <= maxi.y; y++) {
-					float planeDistance = dot(float3(x, y, z) - middle, normal);
-					if (planeDistance <= 1f) {
-						int idx = x * (maxDimensions.z + 1) + z;
-						positions[written++] = new VoxelizedPosition
-						{
-							XZIndex = idx,
-							Y = (short)y
-						};
+					float3 voxel = float3(x, y, z) + 0.5f;
+					float normalDistToTriangle = dot(voxel - a, normalTri);
+					if (abs(normalDistToTriangle) > 0.5f) {
+						continue;
+					}
+					float3 p = voxel - normalTri * normalDistToTriangle;
 
-						if (written == positionsLength) {
-							goto END;
-						}
+
+					float3 v0 = b - a;
+					float3 v1 = c - a;
+					float3 v2 = p - a;
+					float d00 = dot(v0, v0);
+					float d01 = dot(v0, v1);
+					float d11 = dot(v1, v1);
+					float d20 = dot(v2, v0);
+					float d21 = dot(v2, v1);
+					float denom = 1f / (d00 * d11 - d01 * d01);
+					float3 barry;
+					barry.x = (d11 * d20 - d01 * d21) * denom;
+					barry.y = (d00 * d21 - d01 * d20) * denom;
+					barry.z = 1.0f - barry.x - barry.y;
+					if (any(barry < 0 | barry > 1)) {
+						continue;
+					}
+
+					int idx = x * (maxDimensions.z + 1) + z;
+					positions[written++] = new VoxelizedPosition
+					{
+						XZIndex = idx,
+						Y = (short)y
+					};
+
+					if (written == positionsLength) {
+						goto END;
 					}
 				}
 			}
